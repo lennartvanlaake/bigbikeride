9a61ce72534a5e4275c9c241ad7a3fbe62d137c6
Lennart van Laake <lennartvanlaake@gmail.com>
2021-03-17 16:14:08 +0100

    WIP refactor to snowpack and removing tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -12,22 +12,4 @@
 	router.start()
 </script>
 
-<style global lang="postcss">
-
-	/* only apply purgecss on utilities, per Tailwind docs */
-	/* purgecss start ignore */
-	@tailwind base;
-
-	@layer base {
-  button {
-    @apply m-2 p-2 rounded;
-  }
-}
-	@tailwind components;
-	/* purgecss end ignore */
-  
-	@tailwind utilities;
-  
-  </style>
-
 <svelte:component this={page} />
