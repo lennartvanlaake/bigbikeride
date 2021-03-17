8a02e5d765f725816a93530f9c376cb20b101c83
Lennart van Laake <lennartvanlaake@gmail.com>
2021-03-17 17:28:18 +0100

    backend watch should watch backend dir

--- src/javascript/bloglist.js
+++ src/javascript/bloglist.js
@@ -6,5 +6,5 @@ export const blogList = writable([]);
 export async function getBlogs() {
     const call = await fetch(SNOWPACK_PUBLIC_BASE_URL + "/blogs");
     const list = await call.json();
-    blogList.set(list)
+    blogList.set(list)     
 }
